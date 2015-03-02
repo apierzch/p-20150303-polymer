8246eda0b23ba54400806139e6f017f271a5dc78
Adam Pierzchała <pierzchala.adam@gmail.com>
2015-03-02 19:16:01 +0100

    more examples

--- js/main.js
+++ js/main.js
@@ -12,13 +12,20 @@ var appendSlides = function (data) {
     var steps = data;
     var htmltemplate = $('#step-template').html();
     var htmltempl = Handlebars.compile(htmltemplate);
+    var config = steps[0].data;
     steps.forEach(function (step, index) {
         var templ = htmltempl;
-        console.log(step);
+        for(var prop in step.data) {
+            if(config[prop] && Number.isInteger(config[prop])) {
+                config[prop] += step.data[prop];
+            } else {
+                config[prop] = step.data[prop];
+            }
+        }
         $.ajax({
             url: '/steps/' + step.uri,
             success: function (data) {
-                $('.steps').append(templ({file: data, data: step.data,
+                $('.steps').append(templ({file: data, data: config,
                                           class: step.class, id: step.id}));
             },
             async: false
